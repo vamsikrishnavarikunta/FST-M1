c71de508b3b9dab302dcadfb2674eecec7048162
vamsikrishnavarikunta <116861425+vamsikrishnavarikunta@users.noreply.github.com>
2022-12-22 10:51:56 +0530

    Add files via upload

---
+++ Selenium/Activities/Activity10_1.java
@@ -0,0 +1,40 @@
+package org.example;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
+
+public class Activity10_1 {
+    public static void main(String[] args) {
+        WebDriver driver = new FirefoxDriver();
+        Actions actions = new Actions(driver);
+
+        //Open browser
+        driver.get("https://www.training-support.net/selenium/input-events");
+        //Find the cude
+        WebElement cube = driver.findElement(By.id("wrapD3Cube"));
+
+        //Left click
+        actions.click(cube).build().perform();
+        WebElement cubeVal = driver.findElement(By.className("active"));
+        System.out.println("Left Click: " + cubeVal.getText());
+
+        //Double click
+        actions.doubleClick(cube).build().perform();
+        cubeVal = driver.findElement(By.className("active"));
+        System.out.println("Double Click: " + cubeVal.getText());
+
+        //Right click
+        actions.contextClick(cube).build().perform();
+        cubeVal = driver.findElement(By.className("active"));
+        System.out.println("Right Click: " + cubeVal.getText());
+
+        //Close browser
+        driver.close();
+    }
+}
